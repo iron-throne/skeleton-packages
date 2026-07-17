c37daa794a5a404025d41b25617c90b26db8e716
arya <arya.lekshmi@daep.ae>
2026-07-17 12:52:34 +0400

    tabs

--- packages/ui-kit/src/lib/organisms/tabs/Tabs.svelte
+++ packages/ui-kit/src/lib/organisms/tabs/Tabs.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-	import type { ITab } from '@aryagg/types';
+	import Icon from '$lib/atoms/icon/Icon.svelte';
+import type { ITab } from '@aryagg/types';
 	import type { Snippet } from 'svelte';
 
 	let {
 		tabs,
 		active = $bindable(),
 		children,
-		parentClass,
-		tabClass
+		parentKlass,
+		tabKlass
 	}: {
 		tabs: ITab[];
 		active: string;
-		parentClass?: string;
-		tabClass?: string;
+		parentKlass?: string;
+		tabKlass?: string;
 		children?: Snippet;
 	} = $props();
 
@@ -24,7 +25,10 @@
 
 <div>
 	<!-- ITab bar -->
-	<div role="tablist" class="bg-surface-primary flex w-fit rounded-full p-1 text-xs shadow {parentClass}">
+	<div
+		role="tablist"
+		class="bg-surface-primary flex w-fit rounded-full p-1 text-xs shadow {parentKlass}"
+	>
 		{#each tabs as tab (tab.id)}
 			<button
 				role="tab"
@@ -33,14 +37,12 @@
 				onclick={() => {
 					if (!tab.disabled) active = tab.id;
 				}}
-				class="rounded-full px-4 py-1.5 font-medium transition-all duration-300 text-xs {tabClass} {tab.id ===
+				class="rounded-full px-4 py-1.5 font-medium transition-colors duration-300 ease-in-out text-xs {tabKlass} {tab.id ===
 				active
 					? 'bg-primary text-surface-primary shadow-sm'
 					: 'text-secondary hover:text-primary border-0 bg-transparent'}"
 			>
-				{#if tab.icon}
-					<tab.icon width={15} height={15} class="shrink-0" />
-				{/if}
+				<Icon Icon={tab.icon} klass="size-4 shrink-0"/>
 				{tab.label}
 				{#if tab.badge != null}
 					<span
--- packages/ui-kit/src/routes/+page.svelte
+++ packages/ui-kit/src/routes/+page.svelte
@@ -1,5 +1,33 @@
 <script lang="ts">
-	import { Rating } from "$lib";
+	import { Tabs } from '$lib';
+	import { Person, PersonArmsUp } from 'svelte-bootstrap-icons';
+	let active = $state('tasks');
 </script>
 
-<Rating />
+<Tabs
+	tabs={[
+		{
+			id: 'overview',
+			label: 'Project Overview',
+			icon: Person
+		},
+		{
+			id: 'tasks',
+			label: 'Pending Reviews',
+			badge: 12,
+			icon: PersonArmsUp
+		},
+		{
+			id: 'settings',
+			label: 'Configuration',
+			icon: PersonArmsUp
+		},
+		{
+			id: 'archive',
+			label: 'Archived',
+			badge: '34',
+			icon: PersonArmsUp
+		}
+	]}
+	bind:active
+/>
